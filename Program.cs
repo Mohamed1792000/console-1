fc43fa1768b66cac04aa4098535ce2018507d110
Mohamed1792000 <83742425+Mohamed1792000@users.noreply.github.com>
2021-05-06 22:17:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Out.WriteLine("Enter key :");
+            string Key = Console.In.ReadLine();
+            string[] keystr = Key.Split(',');
+            int[] keyint = new int[keystr.Length];
+            for (int i = 0; i < keystr.Length; ++i)
+                keyint[i] = int.Parse(keystr[i]);
+
+
+
+            Console.Out.WriteLine("Enter Messge");
+            string Messge = Console.In.ReadLine();
+            string[] Messgestr = Messge.Split(',');
+            int[] Messgeint = new int[Messgestr.Length];
+            for (int i = 0; i < Messgestr.Length; ++i)
+                Messgeint[i] = int.Parse(Messgestr[i]);
+            {
+
+                int x[1000];
+                for (int i = 0; i < Messge.Length; i++)
+                {
+                    if (Messgeint + keyint <= 10) ;
+                    x = Messgeint + keyint - 10;
+                    x = Messgeint + keyint;
+                }
+
+
+
+            }
+
+        }
+       
+
+
+    }
+}
